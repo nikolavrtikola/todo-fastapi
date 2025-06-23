5dab01bf518ae30baa8448ea7897a17909a9c3b8
kokismoki <zkoljo@yahoo.com>
2025-06-23 14:26:26 +0200

    production postgresql database added

--- database.py
+++ database.py
@@ -1,9 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:test123!@localhost/ToDoDatabase"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:test123!@localhost/ToDoDatabase"
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autoflush=False,autocommit=False,bind=engine)
 Base = declarative_base()
